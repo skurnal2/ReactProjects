d645d3c3befc4bd09c4dd512d9db7ce1436d7a48
skurnal2 <skurnal2@studentmail.nait.ca>
2020-06-28 19:06:39 -0600

    Now Board Maintaining Square's value state

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -4,20 +4,13 @@ import './index.css';
 
 class Square extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
     render() {
       return (
         <button 
           className="square" 
-          onClick={ () => this.setState({value: 'X'}) }
+          onClick={ () => this.props.onClick() }
           >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
@@ -32,7 +25,12 @@ class Square extends React.Component {
     }
 
     renderSquare(i) {
-      return <Square value={this.state.squares[i]} />;
+      return (
+        <Square 
+            value={this.state.squares[i]} 
+            onClick = {() => this.handleClick(i)}
+          />
+      );
     }
   
     render() {
