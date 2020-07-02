aca5a660efd58a90adb2b18cd0f6b4505cb89df0
skurnal2 <skurnal2@studentmail.nait.ca>
2020-07-02 17:50:24 -0600

    Few minor changes

---
+++ sunshine-meme-generator/src/components/prop-panel.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PropPanel = (props) => (
+    <input type="text" defaultValue={props.val} />
+);
+
+export default PropPanel;
--- sunshine-meme-generator/src/index.js
+++ sunshine-meme-generator/src/index.js
@@ -4,19 +4,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Sunshine from './components/sunshine';
 import smiley from './images/smiley.png';
+import PropPanel from './components/prop-panel';
 
 //#endregion
 
 class App extends React.Component {
 
     state = {
-        chosenColor: "#000",
-    }
+        chosenColor: "#e3e3e3",
+    }    
 
     render() {
         return (
-            <div>
-            <input type="text" value="#000"/>
+            <div>            
+            <PropPanel val={this.state.chosenColor} />
             <div id="meme-holder">               
                 <Sunshine fill={this.state.chosenColor} cssclass="sunshine" />
                 <img src={smiley} className="smiley" alt="Sunshine SVG" />
