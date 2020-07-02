659a0af9fdf8dcc9d4ae261703241aabba9a613b
skurnal2 <skurnal2@studentmail.nait.ca>
2020-07-02 16:07:10 -0600

    Added a smiley image

--- sunshine-meme-generator/src/index.js
+++ sunshine-meme-generator/src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import back from './images/sunshine-draft.svg';
+import smiley from './images/smiley.png';
+
 //#endregion
 
 class App extends React.Component {
@@ -10,6 +12,7 @@ class App extends React.Component {
         return (
             <div>
                 <img src={back} className="sunshine" alt="Sunshine SVG" />
+                <img src={smiley} className="smiley" alt="Sunshine SVG" />
             </div>
         );
     }
