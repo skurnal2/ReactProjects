b1fa026d3edaca35ab919fe2aadf90d713d4ead3
skurnal2 <skurnal2@studentmail.nait.ca>
2020-07-01 18:37:30 -0600

    Added few stats

--- geo-covid/src/components/reports.js
+++ geo-covid/src/components/reports.js
@@ -11,10 +11,15 @@ const Reports = ({ reports }) => {
                     <Card.Header>
                         <Accordion.Toggle as={Button} variant="link" eventKey={index}>
                             {report.location}
-                        </Accordion.Toggle>
+                        </Accordion.Toggle> 
                     </Card.Header>
                     <Accordion.Collapse eventKey={index}>
-                        <Card.Body>Hello! I'm the body</Card.Body>
+                        <Card.Body>
+                            Total Cases: {report.total_cases} <br/>
+                            Active Cases: {report.active_cases} <br/>
+                            Recovered Cases: {report.recovered_cases} <br/>
+                            Deaths: {report.deaths}
+                        </Card.Body>
                     </Accordion.Collapse>
                 </Card>
             ))}
