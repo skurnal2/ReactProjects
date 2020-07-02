9781ed3599ce3bf0c50f8e04143f8d688cdd70ca
skurnal2 <skurnal2@studentmail.nait.ca>
2020-07-02 17:33:53 -0600

    Using State variable for color

--- sunshine-meme-generator/src/index.js
+++ sunshine-meme-generator/src/index.js
@@ -2,17 +2,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Sunshine from './sunshine';
+import Sunshine from './components/sunshine';
 import smiley from './images/smiley.png';
 
 //#endregion
 
 class App extends React.Component {
+
+    state = {
+        chosenColor: "#000",
+    }
+
     render() {
         return (
+            <div>
+            <input type="text" value="#000"/>
             <div id="meme-holder">               
-                <Sunshine fill="#f4f4f4" cssclass="sunshine" />
+                <Sunshine fill={this.state.chosenColor} cssclass="sunshine" />
                 <img src={smiley} className="smiley" alt="Sunshine SVG" />
+            </div>
             </div>
         );
     }
