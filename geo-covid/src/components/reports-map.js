2b5da0a03962012816033dfb6ed7c3af87c0ab05
skurnal2 <skurnal2@studentmail.nait.ca>
2020-07-01 20:21:01 -0600

    Added Column Layout

--- geo-covid/src/components/reports-map.js
+++ geo-covid/src/components/reports-map.js
@@ -1,30 +1,41 @@
 import React from 'react';
-import Cards from 'react-bootstrap/Card';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import './slide.scss';
 
 const ReportsMap = ({ reports }) => {
     return (
-        <Cards>
-            {reports.map((report) => (              
+        <Card>
+            {reports.map((report) => (
                 <div>
-                <Card>
-                    <Card.Header>                        
-                            {report.location}                        
-                    </Card.Header>
-                   
+                    <Card>
+                        <Card.Header>
+                            {report.location}
+                        </Card.Header>
+
                         <Card.Body>
-                            Total Cases: {report.total_cases} <br/>
-                            Active Cases: {report.active_cases} <br/>
-                            Recovered Cases: {report.recovered_cases} <br/>
-                            Deaths: {report.deaths}
+                            <Container>
+                                <Row>
+                                    <Col>
+                                        Total Cases: {report.total_cases} <br />
+                                        Active Cases: {report.active_cases} <br />
+                                        Recovered Cases: {report.recovered_cases} <br />
+                                        Deaths: {report.deaths}
+                                    </Col>
+                                    <Col>
+                                        dsa
+                                    </Col>
+                                </Row>
+                            </Container>
                         </Card.Body>
-                    
-                </Card>
+
+                    </Card>
                 </div>
             ))}
-        </Cards>
+        </Card>
     )
 };
 
