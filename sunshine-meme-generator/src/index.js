3917518a6639e2eaf2ba84b0667555256ce3c6a7
skurnal2 <skurnal2@studentmail.nait.ca>
2020-07-02 20:16:13 -0600

    Upload Error Fixed

--- sunshine-meme-generator/src/index.js
+++ sunshine-meme-generator/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Sunshine from './components/sunshine';
 import sourceImg from './images/smiley.png';
+import { isCompositeComponentWithType } from 'react-dom/test-utils';
 //#endregion
 
 class App extends React.Component {
@@ -12,7 +13,7 @@ class App extends React.Component {
         chosenColor: "blue",
         chosenColor2: "#0300b3",
         src: sourceImg
-    }    
+    }
 
     changeColor(value) {
         this.setState({
@@ -27,25 +28,27 @@ class App extends React.Component {
     }
 
     changeImage = (event) => {
-              
-        this.setState({
-            src: URL.createObjectURL(event.target.files[0])
-        });
+
+        if (event.target.files[0]) {
+            this.setState({
+                src: URL.createObjectURL(event.target.files[0])
+            });
+        }
     }
 
     render() {
         return (
-            <div id="content-holder" style={{backgroundColor: this.state.chosenColor2}}>                        
-            <input type="text" placeholder="Color #1" className="box-shadow-3d" defaultValue={this.state.chosenColor} onChange={e => this.changeColor(e.target.value)} /><br/><br/>
-            <input type="text" placeholder="Color #2" className="box-shadow-3d" defaultValue={this.state.chosenColor2} onChange={e => this.changeColor2(e.target.value)} /><br/><br/><br/>
-            <input type='file' className="box-shadow-3d" onChange={this.changeImage} />
-            <div id="meme-holder">               
-                <Sunshine fill={this.state.chosenColor} cssclass="sunshine" />
-                <img src={this.state.src} className="smiley" alt="Center Image" />
-            </div>
-            
-            <h3>
-                Made by Siddharth Kurnal
+            <div id="content-holder" style={{ backgroundColor: this.state.chosenColor2 }}>
+                <input type="text" placeholder="Color #1" className="box-shadow-3d" defaultValue={this.state.chosenColor} onChange={e => this.changeColor(e.target.value)} /><br /><br />
+                <input type="text" placeholder="Color #2" className="box-shadow-3d" defaultValue={this.state.chosenColor2} onChange={e => this.changeColor2(e.target.value)} /><br /><br /><br />
+                <input type='file' className="box-shadow-3d" onChange={this.changeImage} />
+                <div id="meme-holder">
+                    <Sunshine fill={this.state.chosenColor} cssclass="sunshine" />
+                    <img src={this.state.src} className="smiley" alt="Center Image" />
+                </div>
+
+                <h3>
+                    Made by Siddharth Kurnal
             </h3>
             </div>
         );
