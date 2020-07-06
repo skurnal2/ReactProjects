bdc3f6d0159f0486650a0bc57265348c93c88915
skurnal2 <skurnal2@studentmail.nait.ca>
2020-07-06 17:45:36 -0600

    Trying with axios

--- bee-chat-web/src/components/send-chat.js
+++ bee-chat-web/src/components/send-chat.js
@@ -1,39 +1,49 @@
 import React from 'react';
 import { motion } from "framer-motion";
+import axios from 'axios';
 
 class SendChat extends React.Component {
+constructor(props)
+{
+    super(props);
 
-    state = {
+    this.state = {
         send_value: ''
     }
+}
+    
+    changeHandler = e => {
+        this.setState(
+            {
+                send_value: e.target.value,
+                user_id: 1
+            }
+        )
+    } 
 
-    sendPost = () => {     
-        fetch('https://sidprojectsapp.000webhostapp.com/bee-chat-rest-api/api/post/create.php', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                user_id: 1,
-                chat_text: "My value"
+    submitHandler = e => {
+        e.preventDefault();
+        console.log(this.state);
+        axios.post('https://sidprojectsapp.000webhostapp.com/bee-chat-rest-api/api/post/create.php', 
+        this.state)
+            .then(response => {
+                console.log(response);
             })
-        }).then(response => {
-            console.log(response)
-        }).catch(error => {
-            console.log(error)
-        })
-    }
-
-    handleChange(event) {
-        this.setState({ send_value: event.target.value })
+            .catch(error => {
+                console.log(error);
+            })
     }
 
     render() {
+
+        const send_value = this.state.send_value;
+
         return (
             <div id="send-space">
-                <textarea onChange={this.handleChange.bind(this)} />
-                <div id="send" onClick={() => this.sendPost()}>Send</div>
+                <form onSubmit={this.submitHandler}>
+                    <textarea value={send_value} onChange={this.changeHandler} />                    
+                    <input type="submit" id="send" value="Send"/>
+                </form>
             </div>
         );
     }
