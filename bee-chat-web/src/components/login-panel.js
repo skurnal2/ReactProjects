da40cb2638af5e844ec6937e329455fb9ec647c3
skurnal2 <skurnal2@studentmail.nait.ca>
2020-07-08 21:10:05 -0600

    Now setting the cookie for userid
    Will do username and password later

--- bee-chat-web/src/components/login-panel.js
+++ bee-chat-web/src/components/login-panel.js
@@ -10,27 +10,48 @@ class LoginPanel extends React.Component {
 
         this.state = {
             needLogin: true,
+            userid: ""
         }
+    }
+
+    submitHandler = e => {
+        e.preventDefault();
+        cookies.set('user_id', this.state.userid, { path: '/' }) //Setting the cookie
+        this.checkCookie();
+    }
+
+    changeHandler = e => {
+        this.setState(
+            {
+                userid: e.target.value                
+            }
+        )
     }
 
     checkCookie() {
         //Check if user_id exists
         if (cookies.get("user_id")) {
-            //Check if userid is right
+            //Check if userid is right - Will do this later using REST API call
             //if it is not, then give the message and stay on the same page
             //if it is right, then change state of needLogin here and display_chats in parent
+            
         }
     }
 
     render() {
+
+        const user_id = this.state.userid;
+
         if (this.state.needLogin) {
             return (
                 <div id="login-dashboard">
-                    <div id="username-container">
-                        <span>Username</span>
-                        <input type="text"/>
-                    </div>
-                    <input type="submit"/>
+                    <form onSubmit={this.submitHandler}>
+                        <div id="userid-container">
+                            <span>User ID</span>
+                            <input type="text" value={user_id} onChange={this.changeHandler} />
+                        </div>
+                        <input type="submit" />
+                    </form>
                 </div>
             );
         }
