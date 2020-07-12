49aa24a596bd7ecdc362a02c03190254ed694a65
skurnal2 <skurnal2@studentmail.nait.ca>
2020-07-11 18:15:41 -0600

    Minor fixes

--- wallify/components/Detail.js
+++ wallify/components/Detail.js
@@ -4,46 +4,60 @@ import {
     Text,
     ImageBackground,
     View,
-    TouchableOpacity
+    TouchableOpacity,    
 } from 'react-native';
 import { colorsFromUrl } from 'react-native-dominant-color';
+
 
 class List extends React.Component {
     constructor() {
         super();
 
         this.state = {
-            color: '#fff'
+            color: '#fff',
+            image: null
         }
+
     }
-   
+
     componentDidMount() {
         let imgUrl = this.props.route.params.item.download_url;
+        this.state.image = imgUrl;
         colorsFromUrl(imgUrl, (err, colors) => {
-            if(!err) {
+            if (!err) {
                 this.setState({ color: colors.averageColor });
             }
         });
     }
 
-    render() {     
+    setWallify = () => {
+        console.log('hello');
+    }
+
+    render() {
         const item = this.props.route.params.item;
 
         return (
             <View>
-            <ImageBackground
-                source={{ uri: item.download_url }}
-                style={styles.itemImage}>
-                <View style={[
-                    styles.onImage,
-                    {
-                        borderWidth: 4,
-                        borderColor: this.state.color
-                    }
-                ]}>
-                 <Text style={styles.itemText}>{item.author}</Text>
-                </View>                
-            </ImageBackground>            
+                <ImageBackground
+                    source={{ uri: item.download_url }}
+                    style={styles.itemImage}>
+                    <View style={[
+                        styles.onImage,
+                        {
+                            borderStartWidth: 2,
+                            borderBottomWidth: 2,
+                            borderColor: this.state.color
+                        }
+                    ]}>
+                        <Text style={styles.itemText}>{item.author}</Text>
+                        <TouchableOpacity style={[styles.link,{backgroundColor: this.state.color}]}
+                            onPress={() => this.setWallify}
+                        >
+                            <Text style={styles.linkText}>Set Wallpaper</Text>
+                        </TouchableOpacity>
+                    </View>
+                </ImageBackground>
             </View>
         );
     }
@@ -51,25 +65,37 @@ class List extends React.Component {
 
 const styles = StyleSheet.create({
     itemImage: {
-       height: "100%",
-       width: "100%",
-       position: "relative",
+        height: "100%",
+        width: "100%",
+        position: "relative",
     },
     onImage: {
         position: "absolute",
         bottom: 25,
         left: 15,
-        
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
         backgroundColor: "#000",
         opacity: .8,
-        
         padding: 10,
         paddingHorizontal: 20,
         borderRadius: 60
     },
     itemText: {
         color: "white",
-        fontSize: 20,
+        fontSize: 15,
+    },
+    link: {
+
+        marginLeft: 15,
+        padding: 10,
+        borderRadius: 30,
+        opacity: .7
+    },
+    linkText: {
+        color: "white"
     }
 });
 
