5370264df7ec79e9111a62721d7b60bd0b4d0742
skurnal2 <skurnal2@studentmail.nait.ca>
2020-07-15 15:04:27 -0600

    Updates

---
+++ precache-manifest.fcd10c05fc87d751fe036ae4e0975a59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1e17cb01e0226e8847350d6e901260",
+    "url": "/30ProjectsIn60Days/index.html"
+  },
+  {
+    "revision": "ebf86c2d2c5cc4c5ac78",
+    "url": "/30ProjectsIn60Days/static/css/main.17c64863.chunk.css"
+  },
+  {
+    "revision": "8967296996ecf4f6ccee",
+    "url": "/30ProjectsIn60Days/static/js/2.27b9ece7.chunk.js"
+  },
+  {
+    "revision": "3073fcad468d4156b4e5bdea8220e276",
+    "url": "/30ProjectsIn60Days/static/js/2.27b9ece7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf86c2d2c5cc4c5ac78",
+    "url": "/30ProjectsIn60Days/static/js/main.785a9d2d.chunk.js"
+  },
+  {
+    "revision": "e94d26cd3cb758906da6",
+    "url": "/30ProjectsIn60Days/static/js/runtime-main.6707b24d.js"
+  }
+]);
